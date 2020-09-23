9b3549dfb1903a0c5b1a8091aa16f4b55677d09c
Abdul-fatai <staticdev20046@gmail.com>
2020-09-23 11:34:32 +0100

    added htaccess

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,8 +17,6 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
 
-        if(config('app.env') === 'production') {
-            URL::forceScheme('https');
-        }
+        URL::forceScheme('https');
     }
 }
