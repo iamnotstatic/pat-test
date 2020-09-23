19ecc5ce2582d3ea2ad0698837a772ba303dd948
Abdul-fatai <staticdev20046@gmail.com>
2020-09-23 11:25:19 +0100

    made changed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,7 +18,7 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
 
         if(config('app.env') === 'production') {
-            \URL::forceScheme('https');
+            URL::forceScheme('https');
         }
     }
 }
